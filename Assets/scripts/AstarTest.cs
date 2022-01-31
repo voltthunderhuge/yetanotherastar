d51b975456ab58312d948274dd2d8d6eb039ce2b
Graeme Anderson <volttthunderhuge@gmail.com>
2022-01-31 14:16:06 -0800

    make mesh from path

--- Assets/scripts/AstarTest.cs
+++ Assets/scripts/AstarTest.cs
@@ -21,6 +21,9 @@ public class AstarTest : MonoBehaviour
 		_map = new Map(_meshTile, _materialMap);
 		_start = (0, 0);
 		_goal = (_textureMap.width - 1, _textureMap.height - 1);
+
+		_cablePathMesh = new Mesh();
+		_meshFilter = GetComponent<MeshFilter>();
 	}
 
     // Update is called once per frame
@@ -61,6 +64,7 @@ public class AstarTest : MonoBehaviour
 						{
 							Debug.Log("FUuuuuuck!");
 						}
+						MakeMesh();
 					}
 					else
 					{
@@ -72,6 +76,8 @@ public class AstarTest : MonoBehaviour
 
 		Draw();
     }
+
+	
 
 	private void Draw()
 	{
@@ -106,6 +112,156 @@ public class AstarTest : MonoBehaviour
 		}
 	}
 
+	private bool Equal((int x, int z) a, (int x, int z) b)
+	{
+		return a.x == b.x && a.z == b.z;
+	}
+	private (int, int) Dir((int x, int z) a, (int x, int z) b)
+	{
+		return (b.x - a.x, b.z - a.z);
+	}
+
+	private List<(int, int)> Reduce(Dictionary<(int, int), (int, int)> path, (int x, int z) end, (int x, int z) start)
+	{
+		List<(int, int)> corners = new List<(int, int)>();
+
+		(int x, int z) a = end;
+		(int x, int z) b = path[end];
+		(int x, int z) dir = Dir(a, b);
+
+		corners.Add(end);
+		while (true)
+		{
+			a = b;
+			b = path[b];
+
+			(int, int) newDir = Dir(a, b);
+			if (!Equals(newDir, dir))
+			{
+				corners.Add(a);
+				dir = newDir;
+			}
+
+			if (b == start)
+			{
+				break;
+			}
+		}
+		corners.Add(b);
+
+		corners.Reverse();
+
+		return corners;
+	}
+
+	private void MakeMesh()
+	{
+		List<(int x, int z)> reducedPath = Reduce(_cameFrom, _goal, _start);
+
+		if (reducedPath.Count == 0)
+		{
+			return;
+		}
+		int cornerIdx = 0;
+		(int x, int z) ta = reducedPath[cornerIdx];
+		Vector3 a;
+		a.x = Mathf.FloorToInt(ta.x) + 0.5f;
+		a.y = 0.0f;
+		a.z = Mathf.FloorToInt(ta.z) + 0.5f;
+
+		Vector3[] vtx;
+		int[] idx;
+
+		if (reducedPath.Count == 1)
+		{
+			// special case
+			vtx = new Vector3[4];
+			idx = new int[4];
+			return;
+		}
+
+		int vertexCount = (reducedPath.Count - 1) * 4;
+		int indexCount = (reducedPath.Count - 1) * 6;
+		vtx = new Vector3[vertexCount];
+		idx = new int[indexCount];
+
+		(int x, int z) tb;
+		Vector3 b;
+
+		Vector3 vdir = default(Vector3);
+		int vtxIdx = 0;
+		int idxIdx = 0;
+		for (cornerIdx=0; cornerIdx<reducedPath.Count-1; ++cornerIdx)
+		{
+			tb = reducedPath[cornerIdx + 1];
+			b.x = Mathf.FloorToInt(tb.x) + 0.5f;
+			b.y = 0.0f;
+			b.z = Mathf.FloorToInt(tb.z) + 0.5f;
+
+			(int x, int z) dir = Dir(ta, tb);
+			vdir.x = dir.x;
+			vdir.z = dir.z;
+			vdir.Normalize();
+
+			
+			int majorAxis = dir.x == 0 ? 2 : 0;
+			int minorAxis = majorAxis == 0 ? 2 : 0;
+			Debug.Log($"corner {ta} a {a} b {b} maj {majorAxis} min {minorAxis}");
+
+			vtx[vtxIdx][majorAxis] = a[majorAxis] - vdir[majorAxis]*0.5f;
+			vtx[vtxIdx][minorAxis] = a[minorAxis] - 0.5f;
+		
+			vtx[vtxIdx+1][majorAxis] = a[majorAxis] - vdir[majorAxis]*0.5f;
+			vtx[vtxIdx+1][minorAxis] = a[minorAxis] + 0.5f;
+
+			vtx[vtxIdx+2][majorAxis] = b[majorAxis] + vdir[majorAxis]*0.5f;
+			vtx[vtxIdx+2][minorAxis] = b[minorAxis] - 0.5f;
+
+			vtx[vtxIdx + 3][majorAxis] = b[majorAxis] + vdir[majorAxis]*0.5f;
+			vtx[vtxIdx + 3][minorAxis] = b[minorAxis] + 0.5f;
+
+			if ((majorAxis == 0 && Mathf.Sign(vdir[majorAxis]) > 0) ||
+				(majorAxis == 2 && Mathf.Sign(vdir[majorAxis]) < 0))
+			{
+				idx[idxIdx] = vtxIdx;
+				idx[idxIdx + 1] = vtxIdx + 1;
+				idx[idxIdx + 2] = vtxIdx + 2;
+				idx[idxIdx + 3] = vtxIdx + 1;
+				idx[idxIdx + 4] = vtxIdx + 3;
+				idx[idxIdx + 5] = vtxIdx + 2;
+			}
+			else
+			{
+				idx[idxIdx] = vtxIdx;
+				idx[idxIdx + 1] = vtxIdx + 2;
+				idx[idxIdx + 2] = vtxIdx + 1;
+				idx[idxIdx + 3] = vtxIdx + 1;
+				idx[idxIdx + 4] = vtxIdx + 2;
+				idx[idxIdx + 5] = vtxIdx + 3;
+			}
+			ta = tb;
+			a = b;
+
+			vtxIdx += 4;
+			idxIdx += 6;
+		}
+
+		/*Debug.Log("Mesh:");
+		foreach (var v in vtx)
+		{
+			Debug.Log("v: " + v);
+		}
+		foreach (var i in idx)
+		{
+			Debug.Log("i: " + i);
+		}*/
+
+		_cablePathMesh.Clear();
+		_cablePathMesh.vertices = vtx;
+		_cablePathMesh.triangles = idx;
+		_meshFilter.mesh = _cablePathMesh;
+	}
+
 	private void DrawPathNodeAt((int x, int z) tile)
 	{
 		Graphics.DrawMesh(_meshTile, new Vector3(tile.x+0.5f, 0.1f, tile.z+0.5f), Quaternion.identity, _materialPath, 0);
@@ -116,4 +272,7 @@ public class AstarTest : MonoBehaviour
 
 	private (int x, int z) _start, _goal;
 	private Map _map;
+
+	private MeshFilter _meshFilter;
+	private Mesh _cablePathMesh;
 }
